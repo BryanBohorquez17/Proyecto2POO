083614b0ae90da4cdd84bc423047a51e1e6c8582
Bryan Bohorquez <bryanbohorquez14@gmail.com>
2023-11-14 19:51:55 -0500

    se agrego la logica del juego, y la actualizacion de los puntajes en tiempo real

--- src/presentacion/Fprincipal.java
+++ src/presentacion/Fprincipal.java
@@ -1,65 +1,37 @@
 package presentacion;
 
-import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class Fprincipal extends JFrame implements ActionListener {
-    private JLabel lResultado;
-    private JLabel lResultado2;
     private JButton jBotones[][] = new JButton[8][8];
     private String matriz[][] = new String[8][8];
     private String jugadorActual = "B";
+    private JLabel lResultado;
+    private JLabel lResultado2;
 
     public Fprincipal() {
-    	super("imagen");
-    	this.setSize(1,1);
-    	this.setLocationRelativeTo(null);
+        super("Othello POO");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Othello POO");
-        this.setSize(800,800);
+        this.setSize(800, 800);
         this.setLayout(new BorderLayout());
-        
-        JLabel label =new JLabel("",JLabel.CENTER);
-        label.setIcon(new ImageIcon("media/ficha negra.png"));
-        label.setIcon(new ImageIcon("media/ficha blanca.jpg"));
-        this.add(label);
-        this.add(new JLabel("imagen de prueba"));
-        
-        // Etiquetas de resultado
-        this.lResultado = new JLabel("0", SwingConstants.LEFT);
-        this.lResultado.setFont(new Font("Arial", Font.BOLD, 80));
-        this.lResultado.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.lResultado.setVerticalAlignment(SwingConstants.CENTER);
-
-        this.lResultado2 = new JLabel("0", SwingConstants.RIGHT);
-        this.lResultado2.setFont(new Font("Arial", Font.BOLD, 80));
-        this.lResultado2.setVerticalTextPosition(SwingConstants.BOTTOM);
-        this.lResultado2.setVerticalAlignment(SwingConstants.CENTER);
-
-        // Panel para las etiquetas de resultado
-        JPanel pEtiquetas = new JPanel(new BorderLayout());
-        pEtiquetas.add(lResultado, BorderLayout.WEST);
-        pEtiquetas.add(lResultado2, BorderLayout.EAST);
-        this.add(pEtiquetas, BorderLayout.NORTH);
 
         // Panel para los botones
         JPanel pBotones = new JPanel();
         pBotones.setLayout(new GridLayout(8, 8));
         this.add(pBotones, BorderLayout.CENTER);
 
-        // Inicializar la matriz y configurar botones con imágenes
+        // Inicializar botones con color de ficha negra
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 this.matriz[i][j] = "";
@@ -70,14 +42,33 @@ public class Fprincipal extends JFrame implements ActionListener {
             }
         }
 
-        // Inicializar el juego con cuatro fichas en el centro
+  
         matriz[3][3] = "N";
         matriz[3][4] = "B";
         matriz[4][3] = "B";
         matriz[4][4] = "N";
 
-        // Actualizar la interfaz gráfica
+   
+        this.lResultado = new JLabel("0", SwingConstants.LEFT);
+        this.lResultado.setFont(new Font("Arial", Font.BOLD, 80));
+        this.lResultado.setVerticalTextPosition(SwingConstants.BOTTOM);
+        this.lResultado.setVerticalAlignment(SwingConstants.CENTER);
+
+        this.lResultado2 = new JLabel("0", SwingConstants.RIGHT);
+        this.lResultado2.setFont(new Font("Arial", Font.BOLD, 80));
+        this.lResultado2.setVerticalTextPosition(SwingConstants.BOTTOM);
+        this.lResultado2.setVerticalAlignment(SwingConstants.CENTER);
+
+   
+        JPanel pEtiquetas = new JPanel(new BorderLayout());
+        pEtiquetas.add(lResultado, BorderLayout.WEST);
+        pEtiquetas.add(lResultado2, BorderLayout.EAST);
+        this.add(pEtiquetas, BorderLayout.NORTH);
+
+    
         actualizarBotones();
+        actualizarResultados();
+        actualizarPosiblesJugadas();
     }
 
     @Override
@@ -86,7 +77,7 @@ public class Fprincipal extends JFrame implements ActionListener {
         int fila = -1;
         int columna = -1;
 
-        // Encontrar la posición del botón en la matriz
+     
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (botonClic == this.jBotones[i][j]) {
@@ -97,42 +88,115 @@ public class Fprincipal extends JFrame implements ActionListener {
             }
         }
 
-        // Realizar la lógica de movimiento aquí
+   
         if (fila != -1 && columna != -1) {
-            // Verificar si la posición está vacía y es un movimiento válido
+          
             if (matriz[fila][columna].isEmpty() && esMovimientoValido(fila, columna)) {
-                // Realizar el movimiento y actualizar la matriz
+       
                 matriz[fila][columna] = jugadorActual;
 
-                // Invertir las fichas según las reglas del juego
+ 
                 invertirFichas(fila, columna);
 
-                // Cambiar al siguiente jugador
+        
                 jugadorActual = (jugadorActual.equals("B")) ? "N" : "B";
 
-                // Actualizar la interfaz gráfica
-                actualizarBotones();
+                             actualizarBotones();
+                actualizarResultados();
+                actualizarPosiblesJugadas();
             }
         }
     }
 
     private boolean esMovimientoValido(int fila, int columna) {
-        // Lógica para determinar si el movimiento es válido
-        // Implementa tus propias reglas aquí
+
+        if (!matriz[fila][columna].isEmpty()) {
+            return false;
+        }
+
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+               
+                }
+
+                if (hayFichasOponenteEnDireccion(fila, columna, i, j)) {
+                    return true;
+                }
+            }
+        }
 
         return false;
     }
 
+    private boolean hayFichasOponenteEnDireccion(int fila, int columna, int dirFila, int dirColumna) {
+        int filaActual = fila + dirFila;
+        int columnaActual = columna + dirColumna;
+
+        while (filaActual >= 0 && filaActual < 8 && columnaActual >= 0 && columnaActual < 8) {
+            if (matriz[filaActual][columnaActual].equals(jugadorActual)) {
+           
+                return true;
+            }
+
+            if (matriz[filaActual][columnaActual].isEmpty()) {
+      
+                return false;
+            }
+
+          
+            filaActual += dirFila;
+            columnaActual += dirColumna;
+        }
+
+   
+        return false;
+    }
+
     private void invertirFichas(int fila, int columna) {
-        // Lógica para invertir las fichas según las reglas del juego
-        // Implementa tus propias reglas aquí
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue; 
+                }
+
+                if (hayFichasOponenteEnDireccion(fila, columna, i, j)) {
+                    invertirFichasEnDireccion(fila, columna, i, j);
+                }
+            }
+        }
+    }
+
+    private void invertirFichasEnDireccion(int fila, int columna, int dirFila, int dirColumna) {
+        int filaActual = fila + dirFila;
+        int columnaActual = columna + dirColumna;
+
+        while (filaActual >= 0 && filaActual < 8 && columnaActual >= 0 && columnaActual < 8) {
+            if (matriz[filaActual][columnaActual].equals(jugadorActual)) {
+         
+                return;
+            }
+
+    
+            matriz[filaActual][columnaActual] = jugadorActual;
+
+          
+            filaActual += dirFila;
+            columnaActual += dirColumna;
+        }
+    }
+
+    private void actualizarPosiblesJugadas() {
+    	
     }
 
     private void actualizarBotones() {
         ImageIcon iconoBlanco = new ImageIcon("media/ficha blanca.jpg");
         ImageIcon iconoNegro = new ImageIcon("media/ficha negra.png");
 
-        // Configurar botones con imágenes según la matriz
+ 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (matriz[i][j].equals("B")) {
@@ -144,6 +208,26 @@ public class Fprincipal extends JFrame implements ActionListener {
                 }
             }
         }
+    }
+
+    private void actualizarResultados() {
+        int fichasNegras = 0;
+        int fichasBlancas = 0;
+
+      
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (matriz[i][j].equals("B")) {
+                    fichasNegras++;
+                } else if (matriz[i][j].equals("N")) {
+                    fichasBlancas++;
+                }
+            }
+        }
+
+
+        this.lResultado.setText(Integer.toString(fichasNegras));
+        this.lResultado2.setText(Integer.toString(fichasBlancas));
     }
 
     public static void main(String[] args) {
